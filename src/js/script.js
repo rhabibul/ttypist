2bbb1f4299db715659414d901d3761f18dc73bfe
Habibul Rahman <habib2211998.slg@gmail.com>
2022-09-27 21:33:56 +0530

    v0.0.0

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,37 @@
 let wordsInput = document.querySelector("#wordsInput");
 
-const words = Array.from(document.querySelectorAll(".word"));
+const words = document.querySelectorAll(".word");
 
-for (word of words) {
-  console.log(word);
+let active_word = 0;
+let active_letter = 0;
+
+let letter = words[active_word].children;
+
+wordsInput.addEventListener("keydown", handle_keypress);
+
+function handle_keypress(keyevent) {
+  const keypressed = keyevent.key;
+
+  if (keypressed === " ") {
+    if (active_word + 1 == words.length) {
+      words[active_word].classList.remove("active");
+      letter[active_letter].classList.remove("caret");
+      wordsInput.removeEventListener("keydown", handle_keypress, false);
+      // wordsInput.disabled = true;
+      return;
+    }
+    words[active_word].classList.remove("active");
+    ++active_word;
+    words[active_word].classList.add("active");
+    letter[active_letter].classList.remove("caret");
+    active_letter = 0;
+    letter = words[active_word].children;
+    letter[active_letter].classList.add("caret");
+  } else if (keypressed === letter[active_letter].textContent) {
+    words[active_word].classList.remove("incorrect");
+    letter[active_letter].classList.remove("caret");
+    letter[++active_letter].classList.add("caret");
+  } else {
+    words[active_word].classList.toggle("incorrect");
+  }
 }
-
-let output = document.getElementsByTagName("output")[0];
-
-wordsInput.addEventListener("keydown", function (keyevent) {
-  output.textContent = keyevent.key;
-});
