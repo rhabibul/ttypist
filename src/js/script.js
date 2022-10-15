dbc33c45568a95bab33614f9209f501674066e0d
Habibul Rahman <habib2211998.slg@gmail.com>
2022-10-15 15:38:27 +0530

    experimenting

--- src/js/script.js
+++ src/js/script.js
@@ -15,22 +15,35 @@ function handle_keypress(keyevent) {
   if (keypressed === " ") {
     if (active_word + 1 == words.length) {
       words[active_word].classList.remove("active");
+
       letter[active_letter].classList.remove("caret");
+
       wordsInput.removeEventListener("keydown", handle_keypress, false);
-      // wordsInput.disabled = true;
+      wordsInput.disabled = true;
+      // wordsInput.remove();
       return;
     }
+
     words[active_word].classList.remove("active");
+
     ++active_word;
+
     words[active_word].classList.add("active");
+
     letter[active_letter].classList.remove("caret");
+
     active_letter = 0;
+
     letter = words[active_word].children;
+
     letter[active_letter].classList.add("caret");
+    //
   } else if (keypressed === letter[active_letter].textContent) {
     words[active_word].classList.remove("incorrect");
-    letter[active_letter].classList.remove("caret");
-    letter[++active_letter].classList.add("caret");
+
+    letter[active_letter].classList.remove("caret"); // remove caret from correctly typed letter
+
+    letter[++active_letter].classList.add("caret"); // move caret to next letter in the word
   } else {
     words[active_word].classList.add("incorrect");
   }
