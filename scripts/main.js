9c2a888e8e3a57c27a3f46296033a6fd8c02d898
Habibul Rahman <habib2211998.slg@gmail.com>
2022-10-21 01:17:25 +0530

    documentation

--- scripts/main.js
+++ scripts/main.js
@@ -30,7 +30,7 @@ function calculate_speed(startTime, endTime) {
   const totalWordsTyped = totalCharacters / 5;
   const wpm = (totalWordsTyped / totalSeconds) * 60;
 
-  return wpm;
+  return Math.round(wpm);
 }
 
 function handle_keypress(keyevent) {
@@ -44,7 +44,7 @@ function handle_keypress(keyevent) {
 
   const keypressed = keyevent.key;
 
-  if (keypressed === SPACE) { // move to next word if a space is typed
+  if (keypressed === ' ') { // move to next word if a space is typed
 
     if (active_word + 1 == totalWords) { // exit if finished typing all words
       endTime = Date.now();
@@ -55,7 +55,7 @@ function handle_keypress(keyevent) {
       wordsInput.removeEventListener("keydown", handle_keypress, false);
 
       let wpm = calculate_speed(startTime, endTime);
-      speedtag.textContent = `${wpm.toFixed(2)} wpm`;
+      speedtag.textContent = `${wpm}wpm`;
       return;
     }
 
@@ -68,32 +68,41 @@ function handle_keypress(keyevent) {
     words[active_word].classList.add("active"); // highlight the next word
 
     active_letter = 0; // point active_letter to first letter of next word
-    
+
     letters[active_letter].classList.add("caret"); // put caret on first letter of the word
 
-  } else if (keypressed === letters[active_letter].textContent) { // move caret to next letter
-    
+  } else if (keypressed === letters[active_letter].textContent) {
+    // CORRECTLY TYPED: Move caret to next letter
+
     words[active_word].classList.remove("incorrect");
     letters[active_letter].classList.remove("caret");
 
     ++active_letter;
 
     letters[active_letter].classList.add("caret");
-
   } else if (keyevent.metaKey && keypressed == "Backspace") {
-    // set caret to first letter of first word
+    // cmd + backspace
+    // clear all typed words: restart without resetting the timer
 
+    // remove caret and color highlight from current word
     letters[active_letter].classList.remove("caret");
     words[active_word].classList.remove("active");
 
-    for (let i = active_word; i >= 0; --i) {
+    // remove error highlight from current word till first word
+    for (let i = active_word - 1; i >= 0; --i) {
       words[i].classList.remove("incorrect");
+      words[i].classList.add('caret');
+      words[i].classList.remove('caret');
     }
 
+    // reseet variables to initial state
     active_letter = 0;
     active_word = 0;
+
+    // store letters of first word
     letters = words[active_word].children;
 
+    // highlight first word & set care to first letter of first word
     words[active_word].classList.add("active");
     letters[active_letter].classList.add("caret");
   } else if (
@@ -122,32 +131,38 @@ function handle_keypress(keyevent) {
 
     letters[active_letter].classList.add("caret");
   } else if (keypressed == "Backspace") {
+    // BACKSPACE: Take caret one letter back.
+
     words[active_word].classList.remove("incorrect");
 
+    // Take caret to previous letter of the current word as long as there is a
+    // letter before it.
     if (active_letter > 0) {
       letters[active_letter].classList.remove("caret");
       --active_letter;
       letters[active_letter].classList.add("caret");
     }
 
-    // Now user can go all the way to first letter of first word of the sentence
-    // by continously hitting backspace
+    // If caret is at first letter of current word then put the caret after last
+    // letter of previous word.
     if (active_letter == 0 && active_word > 0) {
-      words[active_word].classList.remove("incorrect");
+
+      // remove caret and highlight color from current word
       letters[active_letter].classList.remove("caret");
       words[active_word].classList.remove("active");
 
-      --active_word;
+      --active_word; // go to previous word
+      letters = words[active_word].children; // store letters of previous word
+      words[active_word].classList.add("active"); // highlight the previous word
 
-      words[active_word].classList.add("active");
+      active_letter = letters.length - 1; // point to last letter of previous word
 
-      letters = words[active_word].children;
-
-      active_letter = letters.length - 1;
-
+      // put caret on space character of previous word i.e, caret should appear
+      // after last non-whitespace character of previous word
       letters[active_letter].classList.add("caret");
     }
-  } else {
+  } else  {
+    // INCORRECTLY TYPED: Inserted the wrongly typed letter
     words[active_word].classList.add("incorrect");
   }
 }
