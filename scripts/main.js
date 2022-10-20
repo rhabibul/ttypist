9db9dfe26bc246bfb01a511c8b3e07221aa7b432
Habibul Rahman <habib2211998.slg@gmail.com>
2022-10-20 21:24:46 +0530

    did some documentation on main.js and worked on timer

--- scripts/main.js
+++ scripts/main.js
@@ -3,18 +3,22 @@
 // 2. option to reset everything without reloading the page
 
 const SPACE = " ";
+const speedtag = document.querySelector(".stats > .speed");
 const wordsInput = document.querySelector("#wordsInput");
 const words = document.getElementsByTagName("word");
-const total_words = words.length;
+const totalWords = words.length;
 
-let total_characters = 0;
+let totalCharacters = 0;
 
 for (let word of words) {
-  total_characters += word.children.length;
+  totalCharacters += word.children.length;
 }
 // 1 word = 5 characters
 // 93 / 5 = 18.6 => 19 words of length 5
-// 14s = 93 characters
+//
+// 14s = 19 words
+// 1s = 19/14 words
+// 60s = 19/14 * 60 => 81wpm
 
 let active_word = 0;
 let active_letter = 0;
@@ -23,57 +27,69 @@ let test_started = true;
 let startTime = 0;
 let endTime = 0;
 
-// store letters of first word
-let letters = words[active_word].children;
+let letters = words[active_word].children; // store letters of first word
+
+function calculate_speed(startTime, endTime) {
+
+  const totalSeconds = (endTime - startTime) / 1000;
+  const totalWordsTyped = totalCharacters / 5;
+  const wpm = (totalWordsTyped / totalSeconds) * 60;
+
+  return wpm;
+}
 
 function handle_keypress(keyevent) {
-  if (test_started) {
+
+  keyevent.preventDefault();
+
+  if (test_started) { // start timers as soon as the first letter is typed
     startTime = Date.now();
     test_started = false;
   }
 
-  keyevent.preventDefault();
   const keypressed = keyevent.key;
 
-  // move to next word a space is typed
-  if (keypressed === SPACE) {
-    if (active_word + 1 == total_words) {
+  if (keypressed === SPACE) { // move to next word a space is typed
+
+    if (active_word + 1 == totalWords) { // finished typing all words
       endTime = Date.now();
 
-      const milliseconds = endTime - startTime;
-      console.log(milliseconds);
-      console.log(`${(milliseconds / 1000).toFixed(1)}s`);
-      // enter this block after uses has typed all words
       words[active_word].classList.remove("active");
       letters[active_letter].classList.remove("caret");
 
       wordsInput.removeEventListener("keydown", handle_keypress, false);
+
+      let wpm = calculate_speed(startTime, endTime);
+      speedtag.textContent = `${wpm.toFixed(2)} wpm`;
       return;
     }
 
+    // remove caret and color hightlight from current word
+    letters[active_letter].classList.remove("caret");
     words[active_word].classList.remove("active");
 
     ++active_word;
+    letters = words[active_word].children; // store all letters of the next word
 
-    words[active_word].classList.add("active");
+    words[active_word].classList.add("active"); // make the next word active word
 
-    letters[active_letter].classList.remove("caret");
+    active_letter = 0; // reset active_letter to first letter for next word
 
-    active_letter = 0;
+    letters[active_letter].classList.add("caret"); // put caret on first letter of the word
 
-    letters = words[active_word].children;
-
-    // move caret to next word's first letter
-    letters[active_letter].classList.add("caret");
   } else if (keypressed === letters[active_letter].textContent) {
     // move the caret to next letter in the word
+    
     words[active_word].classList.remove("incorrect");
-
     letters[active_letter].classList.remove("caret");
 
-    letters[++active_letter].classList.add("caret");
+    ++active_letter;
+
+    letters[active_letter].classList.add("caret");
+
   } else if (keyevent.metaKey && keypressed == "Backspace") {
     // set caret to first letter of first word
+
     letters[active_letter].classList.remove("caret");
     words[active_word].classList.remove("active");
 
