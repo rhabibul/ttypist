e5b8c2b0f8b1d0afa04845c85d186b4502ef7ed2
Habibul Rahman <habib2211998.slg@gmail.com>
2022-11-04 02:20:34 +0530

    added a sentence class whose objects will be created when a new test will start

--- scripts/object.js
+++ scripts/object.js
@@ -1,3 +1,5 @@
+const s = "the brown fox jumped over the lazy dog";
+
 class History {
 	#tests;
 	#totaltests;
@@ -55,55 +57,127 @@ class Test {
 		generateRandomWords(N);
 }
 
-class Words {
+class Sentence {
 
-	#words;
-	#length;
-	#currentWordIndex;
-	#currentLetterIndex;
+  #words;
+  #currentWordIndex;
+  #currentLetterIndex;
 
-	constructor(tmpwords) {
-		this.#words = tmpwords;
-		this.#length = this.#words.length;
-		this.#currentWordIndex = 0;
-		this.#currentLetterIndex = 0;
+  constructor(words) {
+
+    if (arguments.length === 0) {
+      this.#words = new Array();
+    } else {
+      this.#words = Array.from(words);
+    }
+
+    this.#currentWordIndex = 0;
+    this.#currentLetterIndex = 0;
+  }
+
+	showWords() {
+		console.log(this.#words.join(' '));
 	}
 
-	get totalwords() {
-		return this.#length;
-	} 	
-	get activeWordIndex() {
-		return this.#currentWordIndex;
-	}		
-	get activeLetterIndex() {
-		return this.#currentLetterIndex;
-	}		
-	get activeWord() {
-		return this.#words[this.activeWordIndex()];
-	} 	
-	get activeLetter() {
-		return this.#words[this.activeWordIndex()][this.activeLetterIndex()];
-	} 	
-	resetIndexes() {
-		this.#currentWordIndex = 0;
-		this.#currentLetterIndex = 0;
+	push_front(...words) {
+		for ( let word of words ) {
+			this.#words.unshift(word);
+		}
 	}
-	resetWordIndex() {
-		this.#currentWordIndex = 0;
+	push_back(...words) {
+		for ( let word of words ) {
+			this.#words.push(word);
+		}
 	}
-	resetLetterIndex() {
-		this.#currentLetterIndex = 0;
+	pop_front() {
+    return this.#words.shift();
 	}
-	goToNextWord() {
-		this.#currentWordIndex++;
+	pop_back() {
+		return this.#words.pop();
 	}
-	goToPreviousWord() {
-		this.#currentWordIndex--;
-	}
-	goToNextLetter() {
-		this.#currentLetterIndex++;
-	}
-	goToPreviousLetter() {
-		this.#currentLetterIndex--;
-	}
+
+  get totalwords() { // return the total number of words present in words array
+    return this.#words.length;
+  }
+
+  get activeWord() { // return the active word
+    try {
+      if (
+        this.#currentWordIndex < 0 ||
+        this.#currentWordIndex >= this.#words.length
+      ) {
+        throw "'currentWordIndex' is out of bound";
+      }
+
+      return this.#words[this.#currentWordIndex];
+    } catch (outofbounderror) {
+      console.error(outofbounderror);
+    }
+  }
+
+  get activeLetter() { // return the active letter
+    try {
+      if (
+        this.#currentWordIndex < 0 ||
+        this.#currentWordIndex >= this.#words.length
+      ) {
+        throw "'currentWordIndex' is out of bound";
+      }
+
+      if (
+        this.#currentLetterIndex < 0 ||
+        this.#currentLetterIndex >= this.#words[this.#currentWordIndex].length
+      ) {
+        throw "'currentLetterIndex' is out of bound";
+      }
+
+      return this.#words[this.#currentWordIndex][this.#currentLetterIndex];
+    } catch (outofbounderror) {
+      console.error(outofbounderror);
+    }
+  }
+
+  get activeWordLength() { // return length of active word
+    return this.#words[this.#currentWordIndex].length;
+  }
+	
+  get activeWordIndex() { // return active word's index
+    return this.#currentWordIndex;
+  }
+  set activeWordIndex(index) { // set new index for active word
+    this.#currentWordIndex = index;
+  }
+
+  get activeLetterIndex() { // return active letter's index
+    return this.#currentLetterIndex;
+  }
+  set activeLetterIndex(index) { // set new index for active letter in active word
+    this.#currentLetterIndex = index;
+  }
+
+  resetIndexes() {
+    this.#currentWordIndex = 0;
+    this.#currentLetterIndex = 0;
+  }
+  resetActiveWordIndex() {
+    this.#currentWordIndex = 0;
+  }
+  resetActiveLetterIndex() {
+    this.#currentLetterIndex = 0;
+  }
+
+  goToNextLetter() {
+    this.#currentLetterIndex++;
+  }
+  goToPreviousLetter() {
+    this.#currentLetterIndex--;
+  }
+  goToNextWord() {
+    this.#currentWordIndex++;
+  }
+  goToPreviousWord() {
+    this.#currentWordIndex--;
+  }
 }
+
+const sentence = new Sentence(s.split(" "));
