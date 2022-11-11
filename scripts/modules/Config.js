3139a5e7e3521adc6b8d32e4f45770fe61d3659b
Habibul Rahman <habib2211998.slg@gmail.com>
2022-11-11 22:52:11 +0530

    information gathering: monkeytype

--- scripts/modules/Config.js
+++ scripts/modules/Config.js
@@ -1,35 +1,20 @@
 // Empty string means none/false/off
 const Config = {
-  theme: {
-    name: "molokai",
-    dark: true,
-    light: false,
-  },
+  liveStats: false,
+  customText: false,
+  guidedLessons: false,
+  includeNumbers: false,
+  includePunctuations: false,
 
-  whitespace: "&nbsp;", // ["&nbsp;", "⸱"]
-
-  only_numpad: false,
-  only_numrow: false,
-
-  live_stats: false,
-  guided_lesson: false,
-
-  time: 60,
-  words: 25,
-  include_numbers: false,
-  include_punctuations: false,
-
-  mode: "time" || "words" || "custom",
-
-  flip_colors: false,
-  highlight_mode: "letter" || "word",
+  highlight_mode: "word", // ["letter", "word"]
+  flip_highlightcolors: false,
 
   // Normal is the classic type test experience.
   // Expert fails the test if you submit (press space) an incorrect word.
   // Master fails if you press a single incorrect key.
-  difficulty: "normal" || "expert" || "master",
+  difficulty: "normal", // ["normal", "expert", "master"]
 
-  backspacing: true, // Enables backspace, (alt/ctrl + backspace) and (meta + backspace)
+  backspace: true, // Enables backspace, (alt/ctrl + backspace) and (meta + backspace)
   forgive_errors: false, // Inserts wrongly typed letters
 
   keyboard: "on", // ["on", "off"]
@@ -37,7 +22,23 @@ const Config = {
   caret_width: "thin", // ["thin", "thick"]
   caret_style: "line", // ["none", "line", "underline", "box", "block"],
 
-  fontSize: "1.5rem",
-  fontWeight: "thin" || "bold",
-  fontFamily: "Roboto Mono, sans-serif",
+  fontsize: "1.4",
+  fontweight: "300",
+  fontfamily: "Roboto Mono",
 };
+
+
+const SelectKeys = {
+  numpad: false,
+  numrow: false,
+  alphanumeric: false,
+  brackets: false, // {[()]}
+  specialsymbols: false, // `~!@#$%^&*()-_=+[{\|;:'".>,</?"}]
+  functionkeys: false, // F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F11, F12
+
+  // ArrowUp, ArrowLeft, ArrowRight, ArrowDown,
+  // KeyW, KeyA, KeyS, KeyD, Numpad8, Numpad4, Numpad6, Numpad2
+  arrowkeys: false,
+};
+
+export { Config, SelectKeys };
