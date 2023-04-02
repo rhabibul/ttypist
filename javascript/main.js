0083760c55c075a13940eec98842c7f6d608a77d
Habibul Rahman <habibul.slg@gmail.com>
2023-04-03 02:04:16 +0530

    put only keydown, keyup, input callback functions in logic.js

---
+++ javascript/main.js
@@ -0,0 +1,28 @@
+import config from "./include/config.js";
+
+import * as ConfigHandler from "./handler/confighandler.js";
+import * as CaretHandler from "./handler/carethandler.js";
+import * as TestAreaElement from "./HTMLElement/TestAreaElement.js";
+import * as Const from "./include/constant.js";
+import * as Misc from "./utils/misc.js"
+
+import Sentence from "./include/sentence.js"
+import Word from "./include/word.js"
+
+
+// only keydown, keyup, input callback functions will be present in logic.js
+// rest code should be put here...
+
+// tolearn
+// 		css grids
+// 		asynchronous javascript
+// 		dom manipulation
+// 		svg
+// 		sass, scss
+// 		json
+// 		sessionStorage & localStorage
+// 		cookies
+// 		setup offline font
+// 		     - roboto mono
+// 		     - mona sans
+// 		     - nunito
