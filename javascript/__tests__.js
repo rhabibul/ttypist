8beb39d9f9749636305d4aec89b73b3c95aea707
Habibul Rahman <habibul.slg@gmail.com>
2023-05-14 00:52:09 +0530

    working on setting buttons ui

--- javascript/__tests__.js
+++ javascript/__tests__.js
@@ -11,7 +11,7 @@ import * as MiscElement from "./HTMLElement/MiscElement.js";
 
 // config object testing
 window.addEventListener("click", (evt) => {
-	
+
 });
 
 window.addEventListener('load', (evt) => {
@@ -39,61 +39,61 @@ SettingElement.fullscreenToggleButton.addEventListener("click", (evt) => {
 	}
 });
 
-// sliders
-const slider_fontsize = document.querySelector("input#slider.fsize");
-const slider_fontweight = document.querySelector("input#slider.fweight");
-const label_fontsize = document.querySelector(".fontsize-value");
-const label_fontweight = document.querySelector(".fontweight-value");
+// // sliders
+// const slider_fontsize = document.querySelector("input#slider.fsize");
+// const slider_fontweight = document.querySelector("input#slider.fweight");
+// const label_fontsize = document.querySelector(".fontsize-value");
+// const label_fontweight = document.querySelector(".fontweight-value");
 
-slider_fontsize.addEventListener("input", () => {
-	MiscElement.root.style.setProperty("--textsize", `${slider_fontsize.value}rem`);
-	label_fontsize.textContent = `${slider_fontsize.value}rem`;
-  Config.text.textsize = slider_fontsize.value;
-});
-slider_fontweight.addEventListener("input", () => {
-	MiscElement.root.style.setProperty("--textweight", `${slider_fontweight.value}`);
-	label_fontweight.textContent = `${slider_fontweight.value}`;
-  Config.text.textweight = slider_fontweight.value;
-});
-
-
-window.addEventListener("offline", (evt) => { 
-	console.log("Tooltip UI: [Icon] You are currently offline. [Refresh Link (in blue)] [cross button]");
-});
-window.addEventListener("online", (evt) => {
-	console.log("Tooltip UI: [Icon] Your Internet connection was restored.  [cross button]");
-})
+// slider_fontsize.addEventListener("input", () => {
+// 	MiscElement.root.style.setProperty("--textsize", `${slider_fontsize.value}rem`);
+// 	label_fontsize.textContent = `${slider_fontsize.value}rem`;
+//   Config.text.textsize = slider_fontsize.value;
+// });
+// slider_fontweight.addEventListener("input", () => {
+// 	MiscElement.root.style.setProperty("--textweight", `${slider_fontweight.value}`);
+// 	label_fontweight.textContent = `${slider_fontweight.value}`;
+//   Config.text.textweight = slider_fontweight.value;
+// });
 
 
-const alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-	'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-];
-const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// window.addEventListener("offline", (evt) => { 
+// 	console.log("Tooltip UI: [Icon] You are currently offline. [Refresh Link (in blue)] [cross button]");
+// });
+// window.addEventListener("online", (evt) => {
+// 	console.log("Tooltip UI: [Icon] Your Internet connection was restored.  [cross button]");
+// })
 
-// try using evt.code & evt.key to determine keyboard layout, use .has(), .get()
-// keyboardLayoutMap
-// 		.entries()
-// 		.keys() 	-> returns values which are equivalent to evt.key of keyboard events
-// 		.values() -> returns values which are equivalent to evt.code of keyboard events
-// 		.get()
-// 	  .has()
-// 	  .size
-//    .forEach((key, code, keyboardLayoutMap) => {})
-navigator.keyboard.getLayoutMap().then((keyboardLayoutMap) => {
+
+// const alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+// 	'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+// ];
+// const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// // try using evt.code & evt.key to determine keyboard layout, use .has(), .get()
+// // keyboardLayoutMap
+// // 		.entries()
+// // 		.keys() 	-> returns values which are equivalent to evt.key of keyboard events
+// // 		.values() -> returns values which are equivalent to evt.code of keyboard events
+// // 		.get()
+// // 	  .has()
+// // 	  .size
+// //    .forEach((key, code, keyboardLayoutMap) => {})
+// navigator.keyboard.getLayoutMap().then((keyboardLayoutMap) => {
 	
-	let s = "", d = "", sym = "";
+// 	let s = "", d = "", sym = "";
 	
-	keyboardLayoutMap.forEach((whatever) => {
-		if ( alphabets.some((char) => whatever === char)) s += whatever;
-		else if ( digits.some((char) => whatever === char) ) d += whatever
-		else sym += whatever;
-	});
+// 	keyboardLayoutMap.forEach((whatever) => {
+// 		if ( alphabets.some((char) => whatever === char)) s += whatever;
+// 		else if ( digits.some((char) => whatever === char) ) d += whatever
+// 		else sym += whatever;
+// 	});
 
-	s = s.split('').sort().join('');
-	d = d.split('').sort().join('');
-	sym = sym.split('').sort().join('');
+// 	s = s.split('').sort().join('');
+// 	d = d.split('').sort().join('');
+// 	sym = sym.split('').sort().join('');
 	
-	console.log(s, s.length);
-	console.log(d, d.length);
-	console.log(sym, sym.length);
-});
+// 	console.log(s, s.length);
+// 	console.log(d, d.length);
+// 	console.log(sym, sym.length);
+// });
