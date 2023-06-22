c1f351566a1a28a3bc66cf2805846cab4a6f7578
Habibul Rahman <habibul.slg@gmail.com>
2023-06-22 21:30:22 +0530

    added click event listeners

--- src/controllers/setting-buttons-controller.js
+++ src/controllers/setting-buttons-controller.js
@@ -2,12 +2,6 @@ import Config from "../../include/config.js"
 import * as TestAreaElement from "../elements/testarea-elements.js";
 import * as SettingsElement from "../elements/settings-elements.js";
 
-// setting
-//    - update internal state (config object)
-//    - update external state (ui changes - buttons)
-//    - update dependencies between among other config options in both config 
-//      object and ui (settings page)
-
 // input field
 TestAreaElement.text.addEventListener("click", () => { TestAreaElement.input.focus(); });
 
