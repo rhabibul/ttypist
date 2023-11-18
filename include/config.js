296d858b503a8f6766441c3b18c33807c4f38263
Habibul Rahman <habibul.slg@gmail.com>
2023-11-18 08:44:06 +0530

    added all docus about config in project docs itself

--- include/config.js
+++ include/config.js
@@ -15,7 +15,7 @@ export const config = {
   arrows: true, // move caret using arrow keys (mythical)
   oppositeshift: false, // use opposite shift keys for shifting, ignore B, Y, ^
   quickend: true, // if disabled then confirm end of test with a space
-  goblind: false, // no errors or incorrect words are highlighted
+  goblind: false, // no errors are highlighted
   strictspace: true, // determines whether to move caret to next word on error.skip and error.replace
   error: {
     off: false, // if errors are disabled caret stops on error
@@ -37,13 +37,13 @@ export const config = {
     meta: false, // backspace + cmd/win
   },
   confidence: {
-    low: true, // use backspace key to delete previous words/letters
-    high: false, // only current word can be edited with bs, cannot go to prev word whether it is correct or incorrect
+    low: true, // use backspace key to delete previous words/letter
+    high: false, // only current word can be deleted using backspace, cannot delete previous word (regardlesss of whether it is correct/incorrect)
     peak: false, // cannot use backspace key at all (no deletion)
   },
   difficulty: {
     ease: true, // classic typing experience, mistakes can be corrected or left behind
-    expert: false, // fails the test if user submit an incorrect word, current word can be edited
+    expert: false, // fails the test if user submit an incorrect word, current word can be edited to correct mistakes
     master: false, // fails the test if user press a single incorrect key
   },
   caret: {
@@ -84,7 +84,7 @@ export const config = {
       digit: false, // include numbers along with words
       punctuation: false, // .?",-';:()[]{}/...
     },
-    capitalization: "off", // off | first | everyfirst | randomly | untidy
+    capitalization: "off", // off | onlyFirstLetter | everyFirstLetter | randomlyFirstLetter | untidy
     whitespace: {
       off: false, // nospace
       style: {
@@ -101,7 +101,7 @@ export const config = {
         letter: true,
         word: false,
       },
-      isflipped: false, // flip primary & secondary highlights
+      isflipped: false, // flip primary & secondary text color highlighting
     },
     color: {
       primary: "#061f2c", // future text, text which needs to be typed (default: more bright)
--- include/container.js
+++ include/container.js
@@ -1,5 +1,4 @@
 export const text = {
-	txt: "",
 	word: [],
 	index: 0,
 
